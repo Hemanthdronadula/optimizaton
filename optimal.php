7f4d6ed451ddbc47274d653288a539e703f6c31e
Hemanthdronadula <108474698+Hemanthdronadula@users.noreply.github.com>
2022-07-01 13:12:38 +0530

    Add files via upload

---
+++ optimal.php
@@ -0,0 +1,24 @@
+<?php 
+function compress($source, $destination, $quality) {
+
+    $info = getimagesize($source);
+
+    if ($info['mime'] == 'image/jpeg') 
+        $image = imagecreatefromjpeg($source);
+
+    elseif ($info['mime'] == 'image/gif') 
+        $image = imagecreatefromgif($source);
+
+    elseif ($info['mime'] == 'image/png') 
+        $image = imagecreatefrompng($source);
+
+    imagejpeg($image, $destination, $quality);
+
+    return $destination;
+}
+
+$source_img = 'sample2.jpg';
+$destination_img = 'sam2.jpg';
+
+$d = compress($source_img, $destination_img, 0);
+?>
